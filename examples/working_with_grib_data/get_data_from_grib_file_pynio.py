2244ba716cfa21e7e7dbcfb90f2ce1b0070e88a8
Mike Kay <mike.kay@spire.com>
2019-12-03 16:45:25 -0500

    Expand PyNIO example

--- examples/working_with_grib_data/get_data_from_grib_file_pynio.py
+++ examples/working_with_grib_data/get_data_from_grib_file_pynio.py
@@ -1,33 +1,74 @@
 """
-Demonstrate how to extract data from several GRIB messages using PyNIO
+Extract point values from GRIB messages
 
-https://www.pyngl.ucar.edu/Nio.shtml
+This program will extract data from a collection of GRIB messages at a single
+geographic location.
+
+Bilinear interpolation is performed on the data to obtain the point values if
+the chosen location is not one of the grid points from the forecast fields.
 """
+from __future__ import print_function
 import argparse
+import csv
+import sys
 
 import Nio
 
 
-def print_variables_for_single_coordinate(filepath, lat, lon):
-    file = Nio.open_file(filepath, mode='r', format='grib')
+# The default set of fields to extract if none are provided on the command line.
+# The simplest way to get this list is to open the file and print the file object:
+#
+# nc = Nio.open_file(...)
+# print(nc)
+# nc.close()
+#
+# Here we default to all of the fields in the basic bundle
+DEF_VARIABLES = (
+    'TMP_P0_L103_GLL0',     # 2-m temperature
+    'DPT_P0_L103_GLL0',     # 2-m dewpoint temperature
+    'RH_P0_L103_GLL0',      # 2-m relative humidity
+    'UGRD_P0_L103_GLL0',    # eastward-component of the wind at 10 meters
+    'VGRD_P0_L103_GLL0',    # northward-component of the wind at 10 meters
+    'GUST_P0_L1_GLL0',      # wind gust speed at 10 meters
+    'PRMSL_P0_L101_GLL0',   # mean sea-level pressure
+    'APCP_P8_L1_GLL0_acc',  # accumulated precipitation amount
+)
 
-    # Retrieve the temperature field from the file variables.
-    temperature_field = file.variables['TMP_P0_L103_GLL0']
-    precipitation_field = file.variables['APCP_P8_L1_GLL0_acc']
+def process_file(filename, variables, lat, lon):
+    # Use PyNio's extended selection to do the interpolation for us.
+    # https://www.pyngl.ucar.edu/NioExtendedSelection.shtml
+    # Here all variables are 2-D. If 3-D (or higher dimension) fields will be extracted
+    # the pattern will need to be adjusted.
+    select = 'lat_0|{lat}i lon_0|{lon}i'.format(lat=lat, lon=lon)
 
-    # Using NIO Extended Selection, select the value for the given lat/lon.
-    print(temperature_field['lat_0|%s lon_0|%s' % (lat, lon)])
-    print(precipitation_field['lat_0|%s lon_0|%s' % (lat, lon)])
+    data = []
+    nc = Nio.open_file(filename, mode='r', format='grib')
+    for name in variables:
+        if name in nc.variables:
+            var = nc.variables[name]
+            value = var[select]
+            units = var.attributes['units']
+        else:
+            value = units = 'Missing'
+        data.append([name, value, units])
+    nc.close()
+
+    writer = csv.writer(sys.stdout)
+    writer.writerow(('Variable', 'Value', 'Units'))
+    writer.writerows(data)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract and print data at a point from a GRIB file using PyNIO')
-    parser.add_argument('filepath', type=str,
-                        help='The path to the file to open')
-    parser.add_argument('--lat', type=float, default=49.6,
-                        help='The latitude of the point')
-    parser.add_argument('--lon', type=float, default=6.1,
-                        help='The longitude of the point')
+    parser = argparse.ArgumentParser(description='Extract point data from forecast files')
+    parser.add_argument('-v', '--variables', type=str,
+                        help='The fields to extract at the point (separate by commas')
+    parser.add_argument('filename', type=str,
+                        help='The file to extract point data from')
+    parser.add_argument('latitude', type=float,
+                        help='The latitude of the extraction point')
+    parser.add_argument('longitude', type=float,
+                        help='The longitude (0-360) of the extraction point')
 
     args = parser.parse_args()
-    print_variables_for_single_coordinate(args.filepath, args.lat, args.lon)
+    variables = args.variables.split(',') if args.variables else DEF_VARIABLES
+    process_file(args.filename, variables, args.latitude, args.longitude)
